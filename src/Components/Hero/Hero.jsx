88b3c06a9058c02bb289601ed40a9530b76f5f74
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-27 18:05:00 +0000

    add image in hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Hero() {
     return(
         <>
-        <section className="bg-blue-50 h-[60vh] w-[80vw] rounded-2xl">
-        
+        <section className="bg-blue-50 h-[60vh] w-[80vw] rounded-2xl flex justify-evenly items-center">
+            <img src="src\assets\Images\plane-globe.png" alt="" srcset="" />
         </section>
         </>
     );
