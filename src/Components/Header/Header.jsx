47e06f211c7fbcbe60d74e45bef8665edd316104
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-28 07:13:32 +0000

    add animation to hero image

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <>
             <header className="bg-blue-800 text-white ">
                 <nav className="h-[15vh] flex justify-between items-center p-4">
-                    <div className="order-1 text-4xl cursor-pointer font-[Martel_Sans]">Flug</div>
+                    <div className="order-1 text-4xl cursor-pointer font-bold font-[Martel_Sans]">Flug</div>
                     <div className="order-2 text-2xl cursor-pointer font-[Radio_Canada]">
                         <ul className="flex gap-2">
                             <li>Home</li>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -15,10 +15,10 @@ export default function Hero() {
     return(
         <>
         <section className="bg-blue-50 h-[60vh] w-[80vw] rounded-2xl flex justify-evenly items-center">
-            <img src="src\assets\Images\plane-globe.png" alt="Plane on a world tour" className="w-[30vw]" />
-            <div>
-                <h1 className="text-blue-950 text-5xl font-extrabold font-[Space_Grotesk]">Check Your Status</h1>
-                <TypeAnimation className="text-right text-3xl font-semibold text-blue-600 font-[Radio_Canada]" sequence={["Check Your Flights", 100, "Check Airport Status", 100]} wrapper="h3" speed={5} repeat={Infinity}></TypeAnimation>
+            <img src="src\assets\Images\plane-globe.png" alt="Plane on a world tour" className="cursor-pointer w-[30vw] hover:-rotate-10 duration-300 hover:scale-[1.1]" />
+            <div className="cursor-pointer ">
+                <h1 className="text-blue-950 text-6xl font-extrabold font-[Space_Grotesk]">Check Your Status</h1>
+                <TypeAnimation className="text-right text-4xl font-semibold text-blue-600 font-[Radio_Canada]" sequence={["Check Your Flights", 100, "Check Airport Status", 100]} wrapper="h3" speed={5} repeat={Infinity}></TypeAnimation>
             </div>
         </section>
         </>
