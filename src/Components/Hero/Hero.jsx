f0b3a9d245dc3e852440976c0e9e7e806d09ff17
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-28 06:25:57 +0000

    add typing animation

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,36 +1,54 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 export default function Hero() {
 
     // Making the Typing Animation
-    const [typeText, setTypeText] = useState("");
-    const [sentenceCount, setSentenceCount] = useState(0);
+
+    const [typeText, setTypeText] = useState("");   // The main typing Text State Variable
+    const [sentenceCount, setSentenceCount] = useState(0);  // State Variable to switch between lines
+    // const [sentenceLength, setSentenceLength] = useState(0);
     let typeTextSentances = [
         "Check your Flights",
         "Check Airport Status"
-    ]
+    ]   // The Lines to display
 
-    const typeTextFunction = () =>{
-        if (sentenceCount === 0){
-            text = typeTextSentances[0].split("");
-            for(let i; i<text.length; i++){
+
+   
+
+    // The Main Function to execute it.
+    const typeTextFunction =  () => {
+        // setSentenceCount(typeTextSentances[0]);
+        setTypeText(" ");
+        if (sentenceCount == 0){
+            let text = typeTextSentances[0].split("");
+            let i = -1;
+            let t = setInterval(()=>{
                 setTypeText(typeText => typeText + text[i]);
-            }
+                i++;
+                if (i == text.length - 1){
+                    clearInterval(t)
+                }
+            }, 100);
             setSentenceCount(1);
-        } else {
-            text = typeTextSentances[1].split("");
-            for(let i; i<text.length; i++){
+        } else{
+            let text = typeTextSentances[1].split("");
+            let i = -1;
+            let t = setInterval(()=>{
                 setTypeText(typeText => typeText + text[i]);
-            }
+                i++;
+                if (i == text.length - 1){
+                    clearInterval(t)
+                }
+            }, 100);
             setSentenceCount(0);
         }
+        
+
     }
 
-    setInterval(() => {
-        typeTextFunction()
-    }, 1000);
+    
 
-
+    
     return(
         <>
         <section className="bg-blue-50 h-[60vh] w-[80vw] rounded-2xl flex justify-evenly items-center">
@@ -38,6 +56,7 @@ export default function Hero() {
             <div>
                 <h1 className="text-blue-950 text-5xl font-bold">Check Your Status</h1>
                 <h3>{typeText}</h3>
+                {/*<button className="bg-red-300 p-4 m-5 text-bold text-white cursor-pointer" onClick={typeTextFunction}>ClickMe</button>*/}
             </div>
         </section>
         </>
