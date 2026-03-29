818063e3f0f33e9ead9d9490daddb744228677a0
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-29 06:02:14 +0000

    update typeing animation text in Hero Component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,26 +3,35 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function Hero() {
 
-   
 
 
-   
 
-    
-    
-    
-    
-    return(
+
+
+
+
+
+
+    return (
         <>
-        <div className="h-[70vh] w-full flex justify-center items-center">
-        <section className="bg-blue-50 h-[60vh] w-[80vw] rounded-2xl flex justify-evenly items-center">
-            <img src="src\assets\Images\plane-globe.png" alt="Plane on a world tour" className="cursor-pointer w-[30vw] hover:-rotate-10 duration-300 hover:scale-[1.1]" />
-            <div className="cursor-pointer ">
-                <h1 className="text-blue-950 text-6xl font-extrabold font-[Space_Grotesk]">Check Your Status</h1>
-                <TypeAnimation className="text-right text-4xl font-semibold text-blue-600 font-[Radio_Canada]" sequence={["Check Your Flights", 100, "Check Airport Status", 100]} wrapper="h3" speed={5} repeat={Infinity}></TypeAnimation>
+            <div className="h-[70vh] w-full flex justify-center items-center">
+                <section className="bg-blue-50 h-[60vh] w-[80vw] rounded-2xl flex justify-evenly items-center">
+                    <img src="src\assets\Images\plane-globe.png" alt="Plane on a world tour" className="cursor-pointer w-[30vw] hover:-rotate-10 duration-300 hover:scale-[1.1]" />
+                    <div className="cursor-pointer ">
+                        <h1 className="text-blue-950 text-6xl font-extrabold font-[Space_Grotesk]">Check Your Status</h1>
+                        <TypeAnimation className="text-right text-4xl font-semibold text-blue-600 font-[Radio_Canada]" sequence={[
+                            "Check Your Flights", 100,
+                            "Check Airport Status", 100,
+                            "Check Your Airlines", 100,
+                            "Check Awesome Airplanes", 100,
+                            "Check Aircrafts Types", 100,
+                            "Check City Status", 100,
+                            "Check Country Status", 100,
+                            "Check Taxes Status", 100
+                        ]} wrapper="h3" speed={5} repeat={Infinity}></TypeAnimation>
+                    </div>
+                </section>
             </div>
-        </section>
-        </div>
         </>
     );
 }
