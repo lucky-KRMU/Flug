530b578b8b551cc8cab59a3a84a570f11f2bfe24
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-29 05:36:32 +0000

    add Card in Search Component

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router';
+
+function SearchCard({Heading, to}) {
+    return(
+        <>
+            <h3 className='h-20 w-50 m-6 text-4xl text-blue-900 flex items-center justify-center border-2 rounded-xl cursor-pointer hover:scale-[1.2] hover:font-semibold hover:border-4 hover:rounded-3xl duration-200 ease-out '><Link to={to}>{Heading}</Link></h3>
+            
+        </>
+    );
+}
 
 function Search() {
   return (
     <>
-    <section id="search" className='h-auto w-auto flex flex-row items-center justify-center'>
+    <section id="search" className='min-h-[70vh] w-auto flex flex-col items-center justify-center'>
     <h2 className='text-4xl py-10 font-semibold font-[Radio_Canada] text-blue-950'>What Do you what to Search?</h2>
+    <div id='search-options' className='grid lg:grid-cols-3 gap-5'>
+        <SearchCard Heading="Flights" to="flights" />
+        <SearchCard Heading="Flights" to="flights" />
+        <SearchCard Heading="Flights" to="flights" />
+        <SearchCard Heading="Flights" to="flights" />
+    </div>
     </section>
     </>
   )
