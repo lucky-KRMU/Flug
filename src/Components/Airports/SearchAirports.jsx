2e1d3d82db3e21a1e61b0cf759c494b43e5aa278
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-29 13:39:58 +0000

    style airport card

--- src/Components/Airports/SearchAirports.jsx
+++ src/Components/Airports/SearchAirports.jsx
@@ -3,11 +3,28 @@ import { FaMagnifyingGlass } from 'react-icons/fa6'
 
 
 
-const AirportCard = () => {
-  return(
+const AirportCard = ({ name, iata, icao, latitude, longitude, geoName, timezone, phone, country, countryCode }) => {
+  return (
     <>
-      <div>
-
+      <div className='w-full h-70 my-2 mb-10 flex items-center justify-center flex-col'>
+        <div id="card" className='w-[80%] p-8 font-[Radio_Canada] border-blue-950 border-4 rounded-2xl'>
+          <h1 className='text-5xl text-blue-950 font-[Space_Grotesk] font-extrabold'>{name}</h1>
+          <h3 className='text-2xl font-semibold text-blue-900'>{iata}</h3>
+          <h3 className='text-2xl font-semibold text-blue-900'>{icao}</h3>
+          <div className='flex justify-between items-center'>
+            <div>
+              <p className='text-xl'><span className='font-semibold text-blue-800'>Latitude: </span>{latitude}</p>
+              <p className='text-xl'><span className='font-semibold text-blue-800'>Longitude: </span>{longitude}</p>
+              <p className='text-xl'><span className='font-semibold text-blue-800'>GeoName: </span>{geoName}</p>
+            </div>
+            <div className='text-right'>
+              <p> <span className='font-semibold text-blue-800'>Time Zone: </span>{timezone}</p>
+              <h3 className='text-2xl'><span className='font-semibold text-blue-800'>Telephone: </span>{phone}</h3>
+              <h3 className='text-3xl font-semibold text-blue-900'>{country}</h3>
+              <p className='text-xl font-semibold'>{countryCode}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
@@ -25,28 +42,40 @@ function SearchAirports() {
   const [longitude, setLongitude] = useState("");
   const [geoName, setGeoName] = useState("");
   const [timezone, setTimezone] = useState("");
+  const [phone, setPhone] = useState("");
   const [country, setCountry] = useState("");
   const [countryCode, setCountryCode] = useState("");
-  
+
 
 
   let url = "../Dummy/dummy_airport_json.json";
 
-  const handleSubmit =  async (e) => {
-   
+  const handleSubmit = async (e) => {
+
     e.preventDefault();
 
     try {
-      
+
       let response = await fetch(url);
       let data = await response.json();
       let airportData = data.data[0];
-      console.log(airportData);
+
+      setName(airportData.airport_name);
+      setIata(airportData.iata_code);
+      setIcao(airportData.icao_code);
+      setLatitude(airportData.latitude);
+      setLongitude(airportData.longitude);
+      setGeoName(airportData.geoname_id);
+      setTimezone(airportData.timezone);
+      setPhone(airportData.phone_number);
+      setCountry(airportData.country_name);
+      setCountryCode(airportData.country_iso2);
+      setFetched(true);
 
     } catch (error) {
-      
+      console.log(error);
     }
-    
+
   }
 
 
@@ -62,6 +91,7 @@ function SearchAirports() {
           </div>
         </form>
       </div>
+      <AirportCard name={name} iata={iata} icao={icao} latitude={latitude} longitude={longitude} geoName={geoName} timezone={timezone} phone={phone} country={country} countryCode={countryCode} />
     </>
   )
 }
