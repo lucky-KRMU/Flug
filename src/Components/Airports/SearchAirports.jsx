041c6abcdd882c2a82394578d31becef1703e933
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-29 12:54:05 +0000

    add handleSubmit in SearchAirport component

--- src/Components/Airports/SearchAirports.jsx
+++ src/Components/Airports/SearchAirports.jsx
@@ -2,10 +2,32 @@ import React from 'react'
 import { FaMagnifyingGlass } from 'react-icons/fa6'
 
 function SearchAirports() {
+
+  let url = "../Dummy/dummy_airport_json.json";
+
+  const handleSubmit =  async (e) => {
+   
+    e.preventDefault();
+
+    try {
+      
+      let response = await fetch(url);
+      let data = await response.json();
+      let airportData = data.data[0];
+      console.log(airportData);
+
+    } catch (error) {
+      
+    }
+    
+  }
+
+
+
   return (
     <>
       <div className='h-[70vh] w-full flex items-center justify-center gap-2'>
-        <form id='search-flight-num'>
+        <form id='search-flight-num' onSubmit={handleSubmit}>
           <h3 className='text-blue-800 py-2 text-4xl font-semibold font-[Radio_Canada]'>Search By Airport Name?</h3>
           <div className='h-20 w-100 flex items-center justify-center gap-2'>
             <input type="text" name="flight-num" id="flight-num" placeholder='Enter the Airport Name Here' className='p-4 bg-blue-50 w-full font-[Radio_Canada] font-semibold text-xl rounded-xl text-blue-950 focus:outline-0' />
