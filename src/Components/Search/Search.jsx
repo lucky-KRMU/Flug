cf22685a067c7d22a9b026f39dc22699c1e21538
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-29 05:59:10 +0000

    add other search options in Search.jsx

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 function SearchCard({Heading, to}) {
     return(
         <>
-            <h3 className='h-20 w-50 m-6 text-4xl text-blue-900 flex items-center justify-center border-2 rounded-xl cursor-pointer hover:scale-[1.2] hover:font-semibold hover:border-4 hover:rounded-3xl duration-200 ease-out '><Link to={to}>{Heading}</Link></h3>
+            <h3 className='h-30 w-60 m-6 text-4xl text-blue-900 flex items-center justify-center border-2 rounded-xl cursor-pointer hover:scale-[1.2] hover:font-semibold hover:border-4 hover:rounded-3xl duration-200 ease-out '><Link to={to}>{Heading}</Link></h3>
             
         </>
     );
@@ -17,9 +17,13 @@ function Search() {
     <h2 className='text-4xl py-10 font-semibold font-[Radio_Canada] text-blue-950'>What Do you what to Search?</h2>
     <div id='search-options' className='grid lg:grid-cols-3 gap-5'>
         <SearchCard Heading="Flights" to="flights" />
-        <SearchCard Heading="Flights" to="flights" />
-        <SearchCard Heading="Flights" to="flights" />
-        <SearchCard Heading="Flights" to="flights" />
+        <SearchCard Heading="Airports" to="" />
+        <SearchCard Heading="Airlines" to="" />
+        <SearchCard Heading="Airplanes" to="" />
+        <SearchCard Heading="Aircraft Types" to="" />
+        <SearchCard Heading="Cities" to="" />
+        <SearchCard Heading="Countries" to="" />
+        <SearchCard Heading="Taxes" to="" />
     </div>
     
     </section>
