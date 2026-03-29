570b34209453588f5a00f6b8c902d7a00f6171ad
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-29 05:11:45 +0000

    update header, add navlinks

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaPlaneUp, FaMagnifyingGlass } from "react-icons/fa6";
+import { NavLink } from "react-router";
 
 const Header = () => {
 
@@ -6,12 +8,15 @@ const Header = () => {
         <>
             <header className="bg-blue-800 text-white ">
                 <nav className="h-[15vh] flex justify-between items-center p-4">
-                    <div className="order-1 text-4xl cursor-pointer font-bold font-[Martel_Sans]">Flug</div>
+                    <div className="order-1 text-4xl cursor-pointer font-bold font-[Martel_Sans] flex gap-2">Flug
+                        <FaPlaneUp />
+                    </div>
                     <div className="order-2 text-2xl cursor-pointer font-[Radio_Canada]">
                         <ul className="flex gap-2">
-                            <li>Home</li>
-                            <li>Airport</li>
-                            <li>Flights</li>
+                            {/* We will be using end in the navlink to make sure that the navlink will not be active in /search/something like this. */}
+                            <li><NavLink to="/" className={({ isActive }) => isActive ? "font-bold" : "text-gray-50"} end>Home</NavLink></li>
+                            <li><NavLink to="flights" className={({ isActive }) => isActive ? "font-bold" : "text-gray-50"} end>Flights</NavLink></li>
+                            <li><NavLink to="search/" className={({ isActive }) => isActive ? "font-bold" : "text-gray-50"} end>Search</NavLink></li>
                         </ul>
                     </div>
                 </nav>
