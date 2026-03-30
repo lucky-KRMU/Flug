b97fc707b07f98cb9f21c2f6eaa74912db7bf569
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-30 13:56:23 +0000

    add state variables

--- src/Components/Airplanes/SearchAirplanes.jsx
+++ src/Components/Airplanes/SearchAirplanes.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchForm from '../SearchForm/SearchForm'
 
 function SearchAirplanes() {
+
+  // State Variables for Airplane
+  const [iata, setIata] = useState("");
+  const [airlineICAO, setAirlineICAO] = useState("");
+  const [airlineIATA, setAirlineIATA] = useState("");
+  const [airplaneIATA, setAirplaneIATA] = useState("");
+  const [constructionNumber, setConstructionNumber] = useState("");
+  const [deliveryDate, setDeliveryDate] = useState("");
+  const [engineCount, setEngineCount] = useState("");
+  const [engineType, SetEngineType] = useState("");
+  const [maidenFlightDate, setMaidenFlightDate] = useState("");
+  const [model, setModel] = useState("");
+  const [registrationNumber, setRegistrationNumber] = useState("");
+  const [testRegistrationNumber, setTestRegistrationNumber] = useState("");
+  const [owner, setOwner] = useState("");
+  const [series, setSeries] = useState("");
+  const [status, setStatus] = useState("");
+  const [productionLine, setProductionLine] = useState("");
+  const [registrationDate, setRegistrationDate] = useState("");
+  const [rollOutDate, setRollOutDate] = useState("");
 
 
   // url for the dummy testing json
@@ -13,13 +33,15 @@ function SearchAirplanes() {
 
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
+    let airplaneData = data.data[0];
+
+
 
   }
 
   return (
     <>
-    <SearchForm searchBy="Airplanes" placeholder="Airplane Name" handleFormSubmit={handleSubmit} />
+    <SearchForm searchBy="Airplanes" placeholder="Registration Number" handleFormSubmit={handleSubmit} />
     </>
   )
 }
