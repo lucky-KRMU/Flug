e88bb6191d9d4d3940c00d58d978efaa9a5c488c
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-30 13:40:58 +0000

    add fetch logic in searchAirplanes

--- src/Components/Airplanes/SearchAirplanes.jsx
+++ src/Components/Airplanes/SearchAirplanes.jsx
@@ -3,8 +3,18 @@ import SearchForm from '../SearchForm/SearchForm'
 
 function SearchAirplanes() {
 
+
+  // url for the dummy testing json
+  let url = "../Dummy/dummy_airplanes_json.json"
+
+  // async function to fetch the data
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+
   }
 
   return (
