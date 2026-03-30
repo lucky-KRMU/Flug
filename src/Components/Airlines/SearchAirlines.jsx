ba7d0a7ac3b0d460388a77cd657ed3abc4f5e17d
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-30 08:48:07 +0000

    add airlines card in SearchAirlines.jsx

--- src/Components/Airlines/SearchAirlines.jsx
+++ src/Components/Airlines/SearchAirlines.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
 import { FaMagnifyingGlass } from 'react-icons/fa6';
 
-const AirlinesCard = () => {
-  return(
+const AirlinesCard = ({ callSign, fleetAge, hub, iata, icao, accNum, name, fleetSize, status, countryCode, foundDate, country }) => {
+  return (
     <>
-    
+      <p>{ callSign }</p>
+      <p>{ fleetAge }</p>
+      <p>{ hub }</p>
+      <p>{ iata }</p>
+      <p>{ icao }</p>
+      <p>{ accNum }</p>
+      <p>{ name }</p>
+      <p>{ fleetSize }</p>
+      <p>{ status }</p>
+      <p>{ countryCode }</p>
+      <p>{ foundDate }</p>
+      <p>{ country }</p>
     </>
   );
 }
@@ -14,7 +25,6 @@ function SearchAirlines() {
 
   // Definging the State Variables
   const [callSign, setCallSign] = useState("");
-  const [id, setId] = useState("");
   const [fleetAge, setFleetAge] = useState("");
   const [hub, setHub] = useState("");
   const [iata, setIata] = useState("");
@@ -31,19 +41,32 @@ function SearchAirlines() {
   let url = "../Dummy/dummy_airlines_json.json";
 
   // function for fetching from api and updating the State/UI
-  const handleSubmit = async (e)=> {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      
+
       let response = await fetch(url);
       let data = await response.json();
       let airlinesData = data.data[0];
-      console.log(airlinesData)
       
+      // updating the State Variables
+      setCallSign(airlinesData.callsign);
+      setFleetAge(airlinesData.fleet_average_age);
+      setHub(airlinesData.hub_code);
+      setIata(airlinesData.iata_code);
+      setIcao(airlinesData.icao_code);
+      setAccNum(airlinesData.iata_prefix_accounting);
+      setName(airlinesData.airline_name);
+      setFleetSize(airlinesData.fleet_size);
+      setStatus(airlinesData.status);
+      setCountryCode(airlinesData.country_iso2);
+      setFoundDate(airlinesData.date_founded);
+      setCountry(airlinesData.country_name);
+
 
     } catch (error) {
-      
+      console.log(error);
     }
 
 
@@ -55,11 +78,12 @@ function SearchAirlines() {
         <form id='search-flight-num' onSubmit={handleSubmit}>
           <h3 className='text-blue-800 py-2 text-4xl font-semibold font-[Radio_Canada]'>Search By Airlines Name?</h3>
           <div className='h-20 w-100 flex items-center justify-center gap-2'>
-            <input type="text" name="flight-num" id="flight-num" placeholder='Enter the Airport Name Here' className='p-4 bg-blue-50 w-full font-[Radio_Canada] font-semibold text-xl rounded-xl text-blue-950 focus:outline-0' />
+            <input type="text" name="flight-num" id="flight-num" placeholder='Enter the Airlines Name Here' className='p-4 bg-blue-50 w-full font-[Radio_Canada] font-semibold text-xl rounded-xl text-blue-950 focus:outline-0' />
             <button type='submit' className='p-4 bg-blue-800 rounded-xl text-white hover:cursor-pointer text-2xl' ><FaMagnifyingGlass /></button>
           </div>
         </form>
       </div>
+      <AirlinesCard callSign={callSign} fleetAge={fleetAge} hub={hub} iata={iata} icao={icao} accNum={accNum} name={name} fleetSize={fleetSize} status={status} countryCode={countryCode} foundDate={foundDate} country={country} />
     </>
   )
 }
