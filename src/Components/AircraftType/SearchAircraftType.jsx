22fda96aa47fb9429c1d0fa214d7e6f350f2938c
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-31 05:11:30 +0000

    add state variables and fetch api logic in SearchAircraftType.jsx

--- src/Components/AircraftType/SearchAircraftType.jsx
+++ src/Components/AircraftType/SearchAircraftType.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SearchForm from "../SearchForm/SearchForm"
 
 function SearchAircraftType() {
 
+    // State Variables
+    const [iata, setIata] = useState("");
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+
+    let url = "/Dummy/dummy_aircraft_type_json.json"
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        let response = await fetch(url);
+        let data = await response.json();
+        
+        let aircraftTypeData = data.data[0];
+        
+
     }
 
   return (
