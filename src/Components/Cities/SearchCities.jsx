98f0981f20060710009cf957e3d13e791b794cac
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-01 14:08:42 +0000

    add fetch data logic in search City

--- src/Components/Cities/SearchCities.jsx
+++ src/Components/Cities/SearchCities.jsx
@@ -3,8 +3,20 @@ import SearchForm from '../SearchForm/SearchForm'
 
 function SearchCities() {
 
+
+    // url
+    let url = "../Dummy/dummy_cities_json.json"
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        let response = await fetch(url);
+        let data = await response.json();
+        let cityData = data.data[0];
+
+        console.log(cityData);
+        
     }
 
     return (
