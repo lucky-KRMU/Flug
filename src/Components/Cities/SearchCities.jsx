c30ff700097524a4658b1b8c6ba142bd1cc01d2b
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-01 14:04:51 +0000

    add Search form in search cities and handleSubmit function

--- src/Components/Cities/SearchCities.jsx
+++ src/Components/Cities/SearchCities.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import SearchForm from '../SearchForm/SearchForm'
 
 function SearchCities() {
-  return (
-    <div>SearchCities</div>
-  )
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <>
+            <SearchForm searchBy="City" placeholder="City Name" handleFormSubmit={handleSubmit} />
+        </>
+    )
 }
 
 export default SearchCities
