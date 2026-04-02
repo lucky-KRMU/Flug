df5289b5465f57721a2e1a832b84e0dee55f33f9
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-02 16:03:17 +0000

    add SearchForm in Search Countries and handleSubmit

--- src/Components/Countries/SearchCountries.jsx
+++ src/Components/Countries/SearchCountries.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import SearchForm from "../SearchForm/SearchForm"
 
 function SearchCountries() {
+
+
+    // handle Submit function to search for the country
+    const handleSubmit = async (e) => {
+        // to prevent default reload
+        e.preventDefault();
+
+        
+
+
+    }
+
   return (
-    <div>SearchCountries</div>
+    <>
+    <SearchForm searchBy="Country" placeholder="Country name" handleFormSubmit={handleSubmit} />
+    </>
   )
 }
 
