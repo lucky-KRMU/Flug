8119f06b667457cce924a96e34398261a7b1bb5c
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-02 16:09:52 +0000

    add fetch logic and add state vars. in SearchCountries.jsx

--- src/Components/Countries/SearchCountries.jsx
+++ src/Components/Countries/SearchCountries.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchForm from "../SearchForm/SearchForm"
 
 function SearchCountries() {
+    
+    // Necessary State Variables
+    const [capital, setCapital] = useState("");
+    const [currCode, setCurrCode] = useState("");
+    const [fips, setFips] = useState("");
+    const [countryIso, setCountryIso] = useState("");
+    const [continent, setContinent] = useState("");
+    const [country, setCountry] = useState("");
+    const [currency, setCurrency] = useState("");
+    const [iso, setIso] = useState("");
+    const [phonePrefix, setPhonePrefix] = useState("");
+    const [population, setPopulation] = useState("");
 
+
+    // url
+    let url = "../Dummy/dummy_countries_json.json" 
 
     // handle Submit function to search for the country
     const handleSubmit = async (e) => {
         // to prevent default reload
         e.preventDefault();
 
-        
+        try {
+            let response = await fetch(url);
+            let data = await response.json();
+
+            let countryData = data.data[0];
+            console.log(countryData);
+
+        } catch (err) {
+            console.log(err);
+        }
+
+
 
 
     }
