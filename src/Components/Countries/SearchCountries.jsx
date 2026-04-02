b5dc0b9f28b6068765455b76747bb483f9d01bf7
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-02 16:17:23 +0000

    update state variables and add CountryCard in SearchCountries.jsx

--- src/Components/Countries/SearchCountries.jsx
+++ src/Components/Countries/SearchCountries.jsx
@@ -1,6 +1,25 @@
 import React, { useState } from 'react'
 import SearchForm from "../SearchForm/SearchForm"
 
+
+const CountryCard = ({capital, currCode, fips, countryIso, continent, country, currency, iso, phonePrefix, population}) => {
+    return (
+        <>
+        <p>{capital}</p>
+        <p>{currCode}</p>
+        <p>{fips}</p>
+        <p>{countryIso}</p>
+        <p>{continent}</p>
+        <p>{country}</p>
+        <p>{currency}</p>
+        <p>{iso}</p>
+        <p>{phonePrefix}</p>
+        <p>{population}</p>
+        </>
+    );
+}
+
+
 function SearchCountries() {
     
     // Necessary State Variables
@@ -29,7 +48,18 @@ function SearchCountries() {
             let data = await response.json();
 
             let countryData = data.data[0];
-            console.log(countryData);
+            
+            // updating the state
+            setCapital(countryData.capital);
+            setCurrCode(countryData.currency_code);
+            setFips(countryData.fips);
+            setCountryIso(countryData.country_iso3);
+            setContinent(countryData.continent);
+            setCountry(countryData.country);
+            setCurrency(countryData.currency_name);
+            setIso(countryData.country_iso_numeric);
+            setPhonePrefix(countryData.phone_prefix);
+            setPopulation(countryData.population);
 
         } catch (err) {
             console.log(err);
@@ -43,6 +73,9 @@ function SearchCountries() {
   return (
     <>
     <SearchForm searchBy="Country" placeholder="Country name" handleFormSubmit={handleSubmit} />
+
+    <CountryCard capital={capital} currCode={currCode} fips={fips} countryIso={countryIso} continent={continent} country={country} currency={currency} iso={iso} phonePrefix={phonePrefix} population={population} />
+
     </>
   )
 }
