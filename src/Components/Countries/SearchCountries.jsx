a1ebd6bf3ab5516869bbd5a83a74a1c6a58cc4da
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-02 16:41:08 +0000

    add styling to CountriesCard in SearchCountries.jsx

--- src/Components/Countries/SearchCountries.jsx
+++ src/Components/Countries/SearchCountries.jsx
@@ -2,26 +2,42 @@ import React, { useState } from 'react'
 import SearchForm from "../SearchForm/SearchForm"
 
 
-const CountryCard = ({capital, currCode, fips, countryIso, continent, country, currency, iso, phonePrefix, population}) => {
+const CountryCard = ({ capital, currCode, fips, countryIso, continent, country, currency, iso, phonePrefix, population }) => {
     return (
         <>
-        <p>{capital}</p>
-        <p>{currCode}</p>
-        <p>{fips}</p>
-        <p>{countryIso}</p>
-        <p>{continent}</p>
-        <p>{country}</p>
-        <p>{currency}</p>
-        <p>{iso}</p>
-        <p>{phonePrefix}</p>
-        <p>{population}</p>
+            <div className='w-full h-auto flex items-center justify-center my-5'>
+                <div className='w-[80%] font-[Radio_Canada] text-xl h-full flex flex-col items-center justify-between gap-10 p-10 border-4 rounded-xl border-blue-950'>
+                    <h1 className='text-6xl text-blue-950 font-semibold font-[Space_Grotesk]'>{country}</h1>
+                    <div className='w-full h-auto flex justify-between items-center'>
+                        <div className=''>
+                            <p><span className='text-xl font-semibold text-blue-800'>Capital: </span>{capital}</p>
+                            <p><span className='text-xl font-semibold text-blue-800'>Continent: </span>{continent}</p>
+                        </div>
+                        <div className='flex flex-col justify-end items-end'>
+                            <p><span className='text-xl font-semibold text-blue-800'>FIPS Code: </span>{fips}</p>
+                            <p><span className='text-xl font-semibold text-blue-800'>Country ISO(3): </span>{countryIso}</p>
+                        </div>
+                    </div>
+                    <div className='w-full h-auto flex justify-between items-center'>
+                        <div className=''>
+                            <p><span className='text-xl font-semibold text-blue-800'>Phone Prefix: </span>{phonePrefix}</p>
+                            <p><span className='text-xl font-semibold text-blue-800'>ISO (Numeric): </span>{iso}</p>
+                            <h3><span className='text-xl font-semibold text-blue-800'>Population: </span>{population}</h3>
+                        </div>
+                        <div className='flex flex-col justify-end items-end'>
+                            <p><span className='text-xl font-semibold text-blue-800'>Currency: </span>{currency}</p>
+                            <p><span className='text-xl font-semibold text-blue-800'>Currency Code: </span>{currCode}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
 
 
 function SearchCountries() {
-    
+
     // Necessary State Variables
     const [capital, setCapital] = useState("");
     const [currCode, setCurrCode] = useState("");
@@ -32,11 +48,11 @@ function SearchCountries() {
     const [currency, setCurrency] = useState("");
     const [iso, setIso] = useState("");
     const [phonePrefix, setPhonePrefix] = useState("");
-    const [population, setPopulation] = useState("");
+    const [population, setPopulation] = useState(0);
 
 
     // url
-    let url = "../Dummy/dummy_countries_json.json" 
+    let url = "../Dummy/dummy_countries_json.json"
 
     // handle Submit function to search for the country
     const handleSubmit = async (e) => {
@@ -48,18 +64,18 @@ function SearchCountries() {
             let data = await response.json();
 
             let countryData = data.data[0];
-            
+
             // updating the state
             setCapital(countryData.capital);
             setCurrCode(countryData.currency_code);
-            setFips(countryData.fips);
+            setFips(countryData.fips_code);
             setCountryIso(countryData.country_iso3);
             setContinent(countryData.continent);
-            setCountry(countryData.country);
+            setCountry(countryData.country_name);
             setCurrency(countryData.currency_name);
             setIso(countryData.country_iso_numeric);
             setPhonePrefix(countryData.phone_prefix);
-            setPopulation(countryData.population);
+            setPopulation(JSON.parse(countryData.population));
 
         } catch (err) {
             console.log(err);
@@ -70,14 +86,14 @@ function SearchCountries() {
 
     }
 
-  return (
-    <>
-    <SearchForm searchBy="Country" placeholder="Country name" handleFormSubmit={handleSubmit} />
+    return (
+        <>
+            <SearchForm searchBy="Country" placeholder="Country name" handleFormSubmit={handleSubmit} />
 
-    <CountryCard capital={capital} currCode={currCode} fips={fips} countryIso={countryIso} continent={continent} country={country} currency={currency} iso={iso} phonePrefix={phonePrefix} population={population} />
+            <CountryCard capital={capital} currCode={currCode} fips={fips} countryIso={countryIso} continent={continent} country={country} currency={currency} iso={iso} phonePrefix={phonePrefix} population={population.toLocaleString('en-US')} />
 
-    </>
-  )
+        </>
+    )
 }
 
 export default SearchCountries
