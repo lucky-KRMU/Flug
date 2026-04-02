38c56315f1df3dbef2febb47d64589f69305004a
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-02 16:50:51 +0000

    update Not Found Page

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { FaPlaneSlash } from 'react-icons/fa6'
 
 function NotFound() {
+
+    useEffect(()=>{
+        document.title = "Not Found | Flug"
+    }, [])
+
     return (
         <>
-            <div>
+            <div className='h-[70vh] w-full flex items-center justify-center gap-2 text-7xl font-[Space_Grotesk] font-bold text-blue-900'>
+                <FaPlaneSlash />
                 <h1>404 Not Found</h1>
             </div>
         </>
