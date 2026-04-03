b667fe1abd7947913b7e2d998bc3d71073bade0f
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-03 10:31:10 +0000

    add actual api in search Flights.jsx

--- src/Components/Flights/SearchFlights.jsx
+++ src/Components/Flights/SearchFlights.jsx
@@ -9,40 +9,40 @@ const FlightInfo = ({ flightDate, flightStatus, depAirport, depAirportIATA, depT
     <>
       <div className='h-auto w-full flex flex-col justify-center items-center '>
         <div className='border-4 my-5 w-[80vw] p-10 border-blue-950 rounded-xl font-[Radio_Canada]'>
-          <h1 className='text-6xl  text-blue-950 font-[Space_Grotesk] font-bold'>{airline}</h1>
-          <h2 className='text-3xl text-blue-900 font-[Radio_Canada] font-semibold'>{flightNumber}</h2>
+          <h1 className='text-6xl  text-blue-950 font-[Space_Grotesk] font-bold'>{airline ? airline : "AIRLINE UNAVAILABLE"}</h1>
+          <h2 className='text-3xl text-blue-900 font-[Radio_Canada] font-semibold'>{flightNumber ? flightNumber : "FLIGHT NUMBER UNAVAILABLE"}</h2>
           <div id="date-card" className='flex justify-between text-xl'>
-            <p className='text-blue-900 font-semibold order-2'>{flightDate}</p>
-            <p className="order-1 font-semibold" >{flightStatus}</p>
+            <p className='text-blue-900 font-semibold order-2'>{flightDate ? flightDate : "DATE NOT AVAILABLE"}</p>
+            <p className="order-1 font-semibold" >{flightStatus ? flightStatus : "NOT AVAILABLE"}</p>
           </div>
           <div id="airportCard" className='flex md:flex-row justify-between items-center my-3 flex-col'>
             <div id="depCard">
-              <h2 className='text-left text-5xl font-bold text-blue-900'>{depAirportIATA}</h2>
-              <h3 className='text-left text-xl font-semibold'>{depAirport}</h3>
+              <h2 className='text-left text-5xl font-bold text-blue-900'>{depAirportIATA ? depAirportIATA : "NOT AVAILABLE"}</h2>
+              <h3 className='text-left text-xl font-semibold'>{depAirport ? depAirport : "NOT AVAILABLE"}</h3>
               <div id="arrTime">
                 <h4 className='text-l font-semibold'>Scheduled Date & Time of Departure:</h4>
                 <div className='flex justify-between items-center'>
-                  <p className='text-left text-l text-blue-900 font-bold'>{depTime.split('T')[0]}</p>
-                  <p className='text-left text-blue-900 font-semibold'>{depTime.split('T')[1].split('+')[0]}</p>
+                  <p className='text-left text-l text-blue-900 font-bold'>{depTime ? depTime.split('T')[0] : "NOT AVAILABLE"}</p>
+                  <p className='text-left text-blue-900 font-semibold'>{depTime ? depTime.split('T')[1].split('+')[0] : "NOT AVAILABLE"}</p>
                 </div>
               </div>
               <div id="arrActualTime">
                 <h4 className='text-l font-semibold'>Actual Date & Time of Departure:</h4>
                 <div className='flex justify-between items-center'>
-                  <p className='text-left text-l text-blue-900 font-bold'>{depActualTime.split('T')[0]}</p>
-                  <p className='text-left text-blue-900 font-semibold'>{depActualTime.split('T')[1].split('+')[0]}</p>
+                  <p className='text-left text-l text-blue-900 font-bold'>{depActualTime ? depActualTime.split('T')[0] : "NOT AVAILABLE"}</p>
+                  <p className='text-left text-blue-900 font-semibold'>{depActualTime ? depActualTime.split('T')[1].split('+')[0] : "NOT AVAILABLE"}</p>
                 </div>
               </div>
             </div>
             <FaPlane className='text-5xl cursor-pointer text-blue-900 hover:text-blue-950 duration-300 ease-in-out hover:scale-[1.1]' />
             <div id="arrCard">
-              <h2 className='text-right text-5xl font-bold text-blue-900'>{arrAirportIATA}</h2>
-              <h3 className='text-right text-xl font-semibold'>{arrAirport}</h3>
+              <h2 className='text-right text-5xl font-bold text-blue-900'>{arrAirportIATA ? arrAirportIATA : "NOT AVAILABLE"}</h2>
+              <h3 className='text-right text-xl font-semibold'>{arrAirport ? arrAirport : "NOT AVAILABLE" }</h3>
               <div id="arrTime">
                 <h4 className='text-l font-semibold'>Scheduled Date & Time of Arrival:</h4>
                 <div className='flex justify-between items-center'>
-                  <p className='text-right text-l text-blue-900 font-bold'>{arrTime.split('T')[0]}</p>
-                  <p className='text-right text-blue-900 font-semibold'>{arrTime.split('T')[1].split('+')[0]}</p>
+                  <p className='text-right text-l text-blue-900 font-bold'>{arrTime ? arrTime.split('T')[0] : "NOT AVAILABLE"}</p>
+                  <p className='text-right text-blue-900 font-semibold'>{arrTime ? arrTime.split('T')[1].split('+')[0] : "NOT AVAILABLE"}</p>
                 </div>
               </div>
               <div id="arrActualTime">
@@ -56,16 +56,16 @@ const FlightInfo = ({ flightDate, flightStatus, depAirport, depAirportIATA, depT
           </div>
           <div id="aircraftDetails" className=' my-5'>
             <h4 className='text-xl font-semibold text-blue-950'>Aircraft Details: </h4>
-            <p><span className='font-semibold text-blue-900'>Registration:</span> {aircraftRegistration}</p>
-            <p><span className='font-semibold text-blue-900'>ICAO:</span> {aircraftICAO}</p>
+            <p><span className='font-semibold text-blue-900'>Registration:</span> {aircraftRegistration ? aircraftRegistration : "NOT AVAILABLE"}</p>
+            <p><span className='font-semibold text-blue-900'>ICAO:</span> {aircraftICAO ? aircraftICAO : "NOT AVAILABLE"}</p>
           </div>
           <div id="liveDetails">
             <h4 className='text-xl font-semibold text-blue-950'>Live Aircraft Details: </h4>
-            <p><span className='font-semibold text-blue-900'>Latitude:</span> {liveLatitude}</p>
-            <p><span className='font-semibold text-blue-900'>Longitude:</span> {liveLongitude}</p>
-            <p><span className='font-semibold text-blue-900'>Altitude:</span> {altitude}</p>
-            <p><span className='font-semibold text-blue-900'>Direction:</span> {direction}</p>
-            <p><span className='font-semibold text-blue-900'>Speed:</span> {speed} knots</p>
+            <p><span className='font-semibold text-blue-900'>Latitude:</span> {liveLatitude ? liveLatitude : "NOT AVAILABLE"}</p>
+            <p><span className='font-semibold text-blue-900'>Longitude:</span> {liveLongitude ? liveLongitude : "NOT AVAILABLE"}</p>
+            <p><span className='font-semibold text-blue-900'>Altitude:</span> {altitude ? altitude : "NOT AVAILABLE"}</p>
+            <p><span className='font-semibold text-blue-900'>Direction:</span> {direction ? direction : "NOT AVAILABLE"}</p>
+            <p><span className='font-semibold text-blue-900'>Speed:</span> {speed ? speed : "NOT AVAILABLE"} knots</p>
           </div>
           {
             landed ?
@@ -99,14 +99,22 @@ function SearchFlights() {
   const [flightNumber, setFlightNumber] = useState("");
   const [aircraftRegistration, setAircraftRegistration] = useState("");
   const [aircraftICAO, setAircraftICAO] = useState("");
-  const [liveLatitude, setLiveLatitude] = useState("");
-  const [liveLongitude, setLiveLongitude] = useState("");
-  const [altitude, setAltitude] = useState("");
-  const [direction, setDirection] = useState("");
-  const [speed, setSpeed] = useState("");
+  const [liveLatitude, setLiveLatitude] = useState(null);
+  const [liveLongitude, setLiveLongitude] = useState(null);
+  const [altitude, setAltitude] = useState(null);
+  const [direction, setDirection] = useState(null);
+  const [speed, setSpeed] = useState(null);
   const [landed, setLanded] = useState(false);
 
-  let url = "https://lucky-krmu.github.io/Flug/Dummy/dummy_flight_search_json.json";
+  const [inputValue, setInputValue] = useState("");
+
+  let API_KEY = import.meta.env.VITE_API_KEY;
+
+  const url = `https://api.aviationstack.com/v1/flights?flight_iata=${inputValue.trim()}&access_key=${API_KEY}`;
+  const options = {method: 'GET', headers: {Accept: "application/json"}}
+
+
+
 
   /*
   We just encountered the bug:
@@ -132,7 +140,7 @@ function SearchFlights() {
 
       setLoading(true);
 
-      let data = await fetch(url);
+      let data = await fetch(url, options);
       let response = await data.json();
       let flightData = response.data[0];
 
@@ -141,29 +149,29 @@ function SearchFlights() {
       // auxiliary variables
       let departure = flightData.departure;
       let arrival = flightData.arrival;
-      let live = flightData.live;
+      // let live = flightData.live;
 
       // Updating the states
-      setFlightDate(flightData.flight_date);
-      setFlightStatus(flightData.flight_status);
-      setDepAirport(departure.airport);
-      setDepAirportIATA(departure.iata);
-      setDepTime(departure.scheduled);
-      setDepActualTime(departure.actual);
-      setArrAirport(arrival.airport);
-      setArrAirportIATA(arrival.iata);
-      setArrTime(arrival.scheduled);
-      setArrActualTime(arrival.estimated);
-      setAirline(flightData.airline.name);
-      setFlightNumber(flightData.flight.iata);
-      setAircraftRegistration(flightData.aircraft.registration);
-      setAircraftICAO(flightData.aircraft.icao);
-      setLiveLatitude(live.latitude);
-      setLiveLongitude(live.longitude);
-      setAltitude(live.altitude);
-      setDirection(live.direction);
-      setSpeed(live.speed_horizontal);
-      setLanded(live.is_ground);
+      setFlightDate(flightData.flight_date ? flightData.flight_date : null);
+      setFlightStatus(flightData.flight_status ? flightData.flight_status : null);
+      setDepAirport(departure.airport ? departure.airport : null);
+      setDepAirportIATA(departure.iata ? departure.iata : null);
+      setDepTime(departure.scheduled ? departure.scheduled : null);
+      setDepActualTime(departure.actual ? departure.actual : null);
+      setArrAirport(arrival.airport ? arrival.airport : null);
+      setArrAirportIATA(arrival.iata ? arrival.iata : null);
+      setArrTime(arrival.scheduled ? arrival.scheduled : null);
+      setArrActualTime(arrival.estimated ? arrival.estimated : null);
+      setAirline(flightData.airline.name ? flightData.airline.name : null);
+      setFlightNumber(flightData.flight.iata ? flightData.flight.iata : null);
+      setAircraftRegistration(flightData.aircraft.registration ? flightData.aircraft.registration : null);
+      setAircraftICAO(flightData.aircraft.icao ? flightData.aircraft.icao : null);
+      // setLiveLatitude(live.latitude);
+      // setLiveLongitude(live.longitude);
+      // setAltitude(live.altitude); 
+      // setDirection(live.direction);
+      // setSpeed(live.speed_horizontal);
+      // setLanded(live.is_ground);
       setDataFetched(true);
       setLoading(false);
 
@@ -183,10 +191,14 @@ function SearchFlights() {
 
   }
 
+  const handleFlight = (e) => {
+    setInputValue(e.target.value.toUpperCase());
+  }
+
 
   return (
     <>
-      <SearchForm searchBy="Flight Name" placeholder="Flight number" handleFormSubmit={handleGetData} />
+      <SearchForm searchBy="Flight Name" placeholder="Flight number" handleFormSubmit={handleGetData} value={inputValue} handleChange={handleFlight} />
       {
         loading ?
           <Loading />
--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { FaMagnifyingGlass } from 'react-icons/fa6'
 
-function SearchForm({searchBy, placeholder,handleFormSubmit, value}) {
+function SearchForm({searchBy, placeholder,handleFormSubmit, value, handleChange}) {
   return (
     <>
     <div className='h-[70vh] w-full flex items-center justify-center gap-2'>
             <form id='search-flight-num' onSubmit={handleFormSubmit}>
               <h3 className='text-blue-800 py-2 text-center text-4xl font-semibold font-[Radio_Canada]'>Search By {searchBy}?</h3>
               <div className='h-20 w-100 flex items-center justify-center gap-2'>
-                <input type="text" name="flight-num" id="flight-num" value={value} placeholder={`Enter the ${placeholder} Here`} className='p-4 bg-blue-50 w-full font-[Radio_Canada] font-semibold text-xl rounded-xl text-blue-950 focus:outline-0' />
+                <input type="text" name="flight-num" id="flight-num" onChange={handleChange} value={value} placeholder={`Enter the ${placeholder} Here`} className='p-4 bg-blue-50 w-full font-[Radio_Canada] font-semibold text-xl rounded-xl text-blue-950 focus:outline-0' />
                 <button type='submit' className='p-4 bg-blue-800 rounded-xl text-white hover:cursor-pointer text-2xl' ><FaMagnifyingGlass /></button>
               </div>
             </form>
