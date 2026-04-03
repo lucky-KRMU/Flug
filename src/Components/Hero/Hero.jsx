c1df36f22039ef908f165eb5883f09b86e7f6ed7
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-03 09:30:04 +0000

    use flug-plane import method in hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { TypeAnimation } from "react-type-animation";
+import Plane from "../../assets/Images/flug-plane.png"
 
 export default function Hero() {
 
@@ -19,7 +20,7 @@ export default function Hero() {
         <>
             <div className="h-[70vh] w-full flex justify-center items-center">
                 <section className="bg-blue-50 h-[60vh] w-[80vw] rounded-2xl flex flex-col justify-evenly items-center md:flex-row sm:p-10">
-                    <img src="src\assets\Images\flug-plane.png" alt="Plane on a world tour" className="cursor-pointer md:w-[30vw] hover:-rotate-10 duration-300 hover:scale-[1.1] sm:w-[50vw]" />
+                    <img src={Plane} alt="Plane on a world tour" className="cursor-pointer md:w-[30vw] hover:-rotate-10 duration-300 hover:scale-[1.1] sm:w-[50vw]" />
                     <div className="cursor-pointer ">
                         <h1 className="text-blue-950 text-6xl font-extrabold font-[Space_Grotesk] sm:text-center">Status? Just Flug It.</h1>
                         <TypeAnimation className="md:text-right md:text-4xl font-semibold text-blue-600 font-[Radio_Canada] sm:text-3xl" sequence={[
