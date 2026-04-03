dac51558170409740db1c29660a2f01fa1bf0877
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-03 10:54:19 +0000

    add actual api in SearchAirlines.jsx

--- src/Components/Airlines/SearchAirlines.jsx
+++ src/Components/Airlines/SearchAirlines.jsx
@@ -58,9 +58,13 @@ function SearchAirlines() {
   const [countryCode, setCountryCode] = useState("");
   const [foundDate, setFoundDate] = useState("");
   const [country, setCountry] = useState("");
+  const [inputVal, setInputVal] = useState("");
 
 
-  let url = "https://lucky-krmu.github.io/Flug/Dummy/dummy_airlines_json.json";
+  let API_KEY = import.meta.env.VITE_API_KEY;
+
+  const url = `https://api.aviationstack.com/v1/airlines?iata_code=${inputVal}&access_key=${API_KEY}`;
+  const options = { method: 'GET', headers: { Accept: 'application/json' } };
 
   // function for fetching from api and updating the State/UI
   const handleSubmit = async (e) => {
@@ -75,18 +79,18 @@ function SearchAirlines() {
       let airlinesData = data.data[0];
 
       // updating the State Variables
-      setCallSign(airlinesData.callsign);
-      setFleetAge(airlinesData.fleet_average_age);
-      setHub(airlinesData.hub_code);
-      setIata(airlinesData.iata_code);
-      setIcao(airlinesData.icao_code);
-      setAccNum(airlinesData.iata_prefix_accounting);
-      setName(airlinesData.airline_name);
-      setFleetSize(airlinesData.fleet_size);
-      setStatus(airlinesData.status);
-      setCountryCode(airlinesData.country_iso2);
-      setFoundDate(airlinesData.date_founded);
-      setCountry(airlinesData.country_name);
+      setCallSign(airlinesData.callsign ? airlinesData.callsign : "UNAVAILABLE");
+      setFleetAge(airlinesData.fleet_average_age ? airlinesData.fleet_average_age : "UNAVAILABLE");
+      setHub(airlinesData.hub_code ? airlinesData.hub_code : "UNAVAILABLE");
+      setIata(airlinesData.iata_code ? airlinesData.iata_code : "UNAVAILABLE");
+      setIcao(airlinesData.icao_code ? airlinesData.icao_code : "UNAVAILABLE");
+      setAccNum(airlinesData.iata_prefix_accounting ? airlinesData.iata_prefix_accounting : "UNAVAILABLE");
+      setName(airlinesData.airline_name ? airlinesData.airline_name : "UNAVAILABLE");
+      setFleetSize(airlinesData.fleet_size ? airlinesData.fleet_size : "UNAVAILABLE");
+      setStatus(airlinesData.status ? airlinesData.status : "UNAVAILABLE");
+      setCountryCode(airlinesData.country_iso2 ? airlinesData.country_iso2 : "UNAVAILABLE");
+      setFoundDate(airlinesData.date_founded ? airlinesData.date_founded : "UNAVAILABLE");
+      setCountry(airlinesData.country_name ? airlinesData.country_name : "UNAVAILABLE");
       setFetched(true);
       setLoading(false);
 
@@ -104,10 +108,15 @@ function SearchAirlines() {
 
   }
 
+  const handleAirlines = (e) => {
+    setInputVal(e.target.value.toUpperCase());
+  }
+
+
   return (
     <>
 
-      <SearchForm searchBy="Airlines Name" placeholder="Airlines Name" handleFormSubmit={handleSubmit} />
+      <SearchForm searchBy="Airlines Name" placeholder="IATA Name" handleFormSubmit={handleSubmit} value={inputVal} handleChange={handleAirlines} />
       {
         loading ?
           <Loading />
